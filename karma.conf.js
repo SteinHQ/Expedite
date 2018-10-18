782e3f8b6ab05a8296bb7ef467af137b0f0b3fbf
Shiven Sinha <shivensinha4@gmail.com>
2018-10-18 12:20:29 +0530

    Specified 'single run' to Karma

--- karma.conf.js
+++ karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function (config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
@@ -68,7 +68,7 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
